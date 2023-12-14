3a9a14808c0213764e5d4893675b1692ee1d0f4f
nikolaiser <nickolayser@gmail.com>
2023-12-14 10:17:39 +0100

    Enable copilot

--- modules/completion/default.nix
+++ modules/completion/default.nix
@@ -41,7 +41,7 @@ in
           }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          -- { name = "copilot" },
+          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
